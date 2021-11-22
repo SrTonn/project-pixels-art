2f92d8abd1030d5ca566c54f18bdf2fcd4a75746
Wellington Rodrigues <ton.rodrigues@icloud.com>
2021-11-22 16:02:40 -0300

    requisito 5 concluido

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-function createPixel(className, parentName) {
+function createPixel(className, parentName, width, height = width) {
   const pixelBoard = document.querySelector(parentName);
   const singleBoxPixel = document.createElement('div');
   singleBoxPixel.className = className;
+  singleBoxPixel.style.width = `${width}px`;
+  singleBoxPixel.style.height = `${height}px`;
+  singleBoxPixel.style.border = '1px solid black';
+
   pixelBoard.appendChild(singleBoxPixel);
 }
 
 for (let i = 0; i < 25; i += 1) {
-  createPixel('pixel', '#pixel-board');
+  createPixel('pixel', '#pixel-board', 40);
 }

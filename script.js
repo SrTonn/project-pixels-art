2fed5488ed838b936408485bc566b9ee92661e63
Wellington Rodrigues <ton.rodrigues@icloud.com>
2021-11-22 17:11:58 -0300

    codigo refatorado para criar os elementos pelo javascript

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+const colorPalleteId = '#color-palette';
+const colorPalleteSize = 45;
+
+function createPalette(className, bgColor, size) {
+  const parent = document.querySelector(colorPalleteId);
+  const color = document.createElement('div');
+
+  color.className = className;
+
+  color.style.display = 'inline-block';
+  color.style.backgroundColor = bgColor;
+
+  color.style.margin = '5px';
+  color.style.border = '1px solid black';
+
+  color.style.width = `${size}px`;
+  color.style.height = `${size}px`;
+
+  parent.appendChild(color);
+}
+createPalette('color selected', 'black', colorPalleteSize);
+createPalette('color', 'green', colorPalleteSize);
+createPalette('color', 'yellow', colorPalleteSize);
+createPalette('color', 'blue', colorPalleteSize);
+
 function createPixel(className, parentName, width, height = width) {
   const pixelBoard = document.querySelector(parentName);
   const singleBoxPixel = document.createElement('div');
@@ -13,11 +38,12 @@ for (let i = 0; i < 25; i += 1) {
   createPixel('pixel', '#pixel-board', 40);
 }
 
-let lastSelected = document.querySelector('#color-palette').firstElementChild;
-console.log(lastSelected);
-document.querySelector('#color-palette').addEventListener('click', (event) => {
+let lastSelected = document.querySelector(colorPalleteId).firstElementChild;
+document.querySelector(colorPalleteId).addEventListener('click', (event) => {
   const element = event.target;
-  element.classList.toggle('selected');
-  lastSelected.classList.toggle('selected');
-  lastSelected = element;
+  if (element.classList.contains('color')) {
+    element.classList.toggle('selected');
+    lastSelected.classList.toggle('selected');
+    lastSelected = element;
+  }
 });
